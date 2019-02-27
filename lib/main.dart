f879807a338c23b6b7d8b2548411b5c89a465fff
Kamil Grzegorzewicz <grzegorzewicz.k@gmail.com>
2019-02-27 13:05:21 +0100

    Basic tic tac toe game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './elements/tile.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,104 +9,165 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Tic tac toe Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: BoardPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BoardPage extends StatefulWidget {
+  BoardPage({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _BoardPageState createState() => _BoardPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BoardPageState extends State<BoardPage> {
+  List<TileType> _tiles = List.filled(9, TileType.EMPTY);
+  TileType _currentTile = TileType.CROSS;
 
-  void _incrementCounter() {
+  TileType _checkWinner() {
+    List<List<int>> indexes = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    if (indexes.indexWhere(
+            (index) => index.every((i) => _tiles[i] == TileType.CIRCLE)) >
+        -1) {
+      return TileType.CIRCLE;
+    } else if (indexes.indexWhere(
+            (index) => index.every((i) => _tiles[i] == TileType.CROSS)) >
+        -1) {
+      return TileType.CROSS;
+    }
+
+    return null;
+  }
+
+  void _reset() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentTile = TileType.CROSS;
+      _tiles = List.filled(9, TileType.EMPTY);
     });
+  }
+
+  void _update({BuildContext context, int index}) {
+    if (_tiles[index] != TileType.EMPTY) return;
+
+    _tiles[index] = _currentTile;
+
+    final winner = _checkWinner();
+
+    setState(() {
+      _currentTile =
+          _currentTile == TileType.CROSS ? TileType.CIRCLE : TileType.CROSS;
+    });
+
+    final isWinner = winner != null;
+    final isTie = _tiles.every((tile) => tile != TileType.EMPTY);
+
+    if (isWinner) {
+      showDialog(
+          context: context,
+          builder: (context) =>
+              _buildWinnerDialog(context: context, winner: winner),
+          barrierDismissible: false);
+    } else if (isTie) {
+      showDialog(
+          context: context,
+          builder: _buildTieDialog,
+          barrierDismissible: false);
+    }
+  }
+
+  Widget _buildWinnerDialog({BuildContext context, TileType winner}) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      title: Text(
+        'End of game',
+        textAlign: TextAlign.center,
+      ),
+      content: Text(
+        '${winner == TileType.CIRCLE ? 'Circle' : 'Cross'} did win',
+        textAlign: TextAlign.center,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Reset'),
+          onPressed: () {
+            _reset();
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildTieDialog(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      title: Text(
+        'End of game',
+        textAlign: TextAlign.center,
+      ),
+      content: Text(
+        'No one win',
+        textAlign: TextAlign.center,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Reset'),
+          onPressed: () {
+            _reset();
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Container(
+                height: 200,
+                width: 200,
+                color: Colors.brown,
+                child: GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 5,
+                      crossAxisSpacing: 5),
+                  padding: EdgeInsets.all(5),
+                  itemBuilder: (context, index) {
+                    return Tile(
+                        type: _tiles[index],
+                        onPress: () {
+                          _update(context: context, index: index);
+                        });
+                  },
+                )),
+            Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                    '${_currentTile == TileType.CIRCLE ? 'Circle' : 'Cross'} should make the move')),
+            FlatButton(child: Text('Reset'), onPressed: _reset)
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
